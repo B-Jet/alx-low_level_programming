7deba95b6ea5f4148e59e9b133e2cd237d959230
B-Jet <danielanugwajustice@gmail.com>
2023-09-26 01:28:36 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,17 @@
-#include <stddio.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <string.h>
 
 /**
- * main - Entry point
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
  *
- * Return: Always return 1
+ * Return: Always 1.
  */
-
 int main(void)
 {
-	const char *quote ="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, quote, strlen(quote));
+	write(2,
+			"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+			59);
 	return (1);
 }
